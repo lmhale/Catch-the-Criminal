2a28e01a7bd299fcbfb90201a7bb5c46f7114161
Lauren M Hale <laurenmhale@Laurens-MacBook-Pro.local>
2018-03-29 19:04:03 -0400

    first commit

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
     let Landing = $('<div/>');
-    //Landing.className = 'container';
 
 Landing.attr('id','container');
 Landing.appendTo('body')
@@ -21,37 +20,88 @@ detective.appendTo('#container')
 detective.attr('src','images/detective.jpg');
 detective.attr('class','detective')
 
+//map 
 let map = $('<img/>')
 map.attr('src','images/map.gif');
 map.attr('class','map');
 map.appendTo('#board');
 
-let textBox = $('<div/>').attr('class','textBox');
-textBox.appendTo('#board')
+
+
+
+        
 
 let formDiv = $('<div/>').attr('id','form');
 $(formDiv).appendTo('#container');
-
 $('#myForm').appendTo(formDiv);
 
 
     $("#myForm").submit(function(e) {
         $("#container").hide();
         $("#board").show();
-        
+       
         e.preventDefault();
        
     });
 
+    var message;
+    var user;
+    
+    $('#messageBox').appendTo('#board');
+
     $("#myForm").submit(function(e) {
+       $('#messageBox').show();
+        user = $('#input_name').val(); 
+        message = `<span class="span">Welcome detective ${user}! </span> <br/> <p>The international jewel thief was spotted most recently in the open-air markets of Tehran. You better get to work!</p> `; 
+       
+        $(message).appendTo('#messageBox');
+
+       $("#contBtn").show();
         e.preventDefault();
-        var user = $('#input_name').val();
-        $('.textBox').text("hello " + user);
-        
+
+});
+
+
+
+$("#contBtn").click(function(){
+    $("#messageBox").hide();
+   
+});
+
+ 
+
+
+let contBtn = $("#contBtn");
+
+
+$("#Iran").click((e) => {
+    $("#messageBox").show();
+    //$("#contBtn").show(); 
+    message="hello there";
+    $('#messageBox').html(message);
+    //console.log('#contBtn');
+    $("#messageBox").append(contBtn);
+    e.preventDefault();
+    $("#contBtn").click(function(){
+        $("#messageBox").hide();
+       
     });
 
+   
+});
 
+$("#test2").click(function(){
+    $("#messageBox").show(); 
+    message="hiiiiiiii";
+    $('#messageBox').html(message);
+    $("#contBtn").show();
+    
+    e.preventDefault();
+});
 
+let Iran = $('#Iran').appendTo('#board');
+
+let countries = [Iran];
 
 
 
